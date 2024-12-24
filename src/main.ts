a713c862ed9360303b89d878123e8f4f00d17abb
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2024-12-24 19:24:19 +0100

    Added drawing file displays

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,44 @@ async function textInputChanged(event: Event) {
   currentSimilarStartDirectories = await invoke("search_starting_directories", {
     text,
   });
+  drawSimilarStartingDirectories();
+function drawSimilarStartingDirectories() {
+  var layout = document.getElementById("filesDisplayLayout") as HTMLElement;
+  // clear old childs
+  layout.innerHTML = "";
+  var displaysLength = Math.min(
+    currentSimilarStartDirectories.length,
+    maxSimilarStartDictionariesLength
+  );
+  for (let index = 0; index < displaysLength; index++) {
+    const text = currentSimilarStartDirectories[index];
+    createFileDisplay(
+      text,
+      "/src/assets/typescript.svg",
+      selectedDirIndex == index
+    );
+  }
 }
+
+function createFileDisplay(name: string, iconSrc: string, highlight: boolean) {
+  var layout = document.getElementById("filesDisplayLayout") as HTMLElement;
+
+  const fileDisplay = document.createElement("fileDisplay");
+  fileDisplay.className = "fileDisplay";
+  if (highlight) fileDisplay.style.border = "5px solid #555555";
+  const icon = document.createElement("img");
+  icon.src = iconSrc;
+  icon.className = "fileIcon";
+  fileDisplay.append(icon);
+
+  const fileName = document.createElement("fileName");
+  fileName.textContent = name;
+  fileName.className = "fileName";
+  fileDisplay.append(fileName);
+
+  layout.append(fileDisplay);
+}
+
 debug("Begin");
 focus();
 
