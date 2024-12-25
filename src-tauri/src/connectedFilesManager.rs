be95c7a2ba1d632284bc573169c8dbf04b3a87ed
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2024-12-25 21:17:54 +0100

    Added way to extract extension of file

--- src-tauri/src/connectedFilesManager.rs
+++ src-tauri/src/connectedFilesManager.rs
@@ -1,19 +1,26 @@
 use std::fs;
-
-use serde::de::value;
 #[tauri::command]
 pub fn get_connected_files(currentPath: String) -> Vec<fileStat> {
+    println!("get_connected_files-path {}", currentPath);
     let mut return_vec: Vec<fileStat> = Vec::new();
-    let paths = fs::read_dir(currentPath).unwrap();
+    let paths = fs::read_dir(&currentPath).unwrap();
 
     for dir in paths {
-        let path = dir.unwrap().path().display().to_string();
-        let splittedPath = path.split("/");
+        let path = dir.unwrap().path().as_path().to_str().unwrap().to_string();
+        let splittedPath = path.split(&['/']);
         let fileName = splittedPath.last().unwrap();
-        println!("test file name: {} full path: {}", fileName, path);
+        let nameAndExtension = fileName.split(".");
+
+        let extension: String;
+        if nameAndExtension.clone().count() != 1 as usize {
+            extension = nameAndExtension.last().unwrap().to_string();
+        } else {
+            extension = "".to_string();
+        }
         return_vec.push(fileStat {
             path: path.to_string(),
             name: fileName.to_string(),
+            extension,
         });
     }
 
@@ -23,4 +30,5 @@ pub fn get_connected_files(currentPath: String) -> Vec<fileStat> {
 pub struct fileStat {
     pub path: String,
     pub name: String,
+    pub extension: String,
 }
